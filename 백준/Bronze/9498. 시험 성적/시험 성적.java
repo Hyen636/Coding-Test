b9fcab0e5e9bbee27375d85c6f311fe13d61ae81
Hyen <94895962+Hyen636@users.noreply.github.com>
2022-08-26 13:00:06 +0900

    [Bronze V] Title: 시험 성적, Time: 220 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		
+		in.close();
+        
+		if(90<=A) System.out.println("A");
+		else if(80<=A) System.out.println("B");
+        else if(70<=A) System.out.println("C");
+        else if(60<=A) System.out.println("D");
+		else System.out.println("F");
+	}
+}
